63c4e640e99ab4e56eb417bda4797a68afe7d09d
Yury Suponeu <usuponev@gmail.com>
2024-08-07 13:20:41 +0400

    add view issue scenario

--- youtrack.js
+++ youtrack.js
@@ -1,19 +1,18 @@
 import http from "k6/http";
 import { SharedArray } from "k6/data";
-import { HOSTS } from "../utils/hosts.js";
-import papaparse from "../utils/papaparse.min.js";
-import { getEnvVar, randomItem, randomString } from "../utils/utils.js";
+import { HOSTS } from "./utils/hosts.js";
+import { ENDPOINTS } from "./utils/enpoints.js";
+import papaparse from "./utils/papaparse.min.js";
+import { getEnvVar, randomItem, randomString } from "./utils/utils.js";
 import { sleep } from "k6";
 
 // Parameters setup
 const ENV_ = getEnvVar("ENV", "remote");
 const BASE_URL = getEnvVar("BASE_URL", HOSTS[ENV_]);
-const FEEDS_DIR = "../feeds";
+const FEEDS_DIR = "./feeds";
 const API_PREFIX = "api";
-const CREATE_ISSUE = "/issues";
-const UPDATE_ISSUE = "/issues";
-const VIEW_ISSUE = "/issues";
-const SEARCH = "/issues";
+const GET_PREFIX = "GET ";
+const POST_PREFIX = "POST ";
 const HTTP_TIMEOUT = "10s"; // override default timeout 60s
 const PRE_ALLOCATED_VUS_10 = 10; // low load rate
 const PRE_ALLOCATED_VUS_50 = 50; // high load rate
@@ -25,9 +24,9 @@ const RATE_UPDATE_ISSUE = Math.ceil(15 * X_LOAD);
 const RATE_VIEW_ISSUE = Math.ceil(50 * X_LOAD);
 const RATE_SEARCH = Math.ceil(17 * X_LOAD);
 // Load Profile
-const RAMP_UP = getEnvVar("RAMP_UP", "60s");
-const HOLD_RATE = getEnvVar("HOLD_RATE", "60s");
-const TEAR_DOWN = getEnvVar("TEAR_DOWN", "60s");
+const RAMP_UP = getEnvVar("RAMP_UP", "300s");
+const HOLD_RATE = getEnvVar("HOLD_RATE", "10s");
+const TEAR_DOWN = getEnvVar("TEAR_DOWN", "10s");
 
 /*
  * Load profile:
@@ -42,7 +41,7 @@ export const options = {
   scenarios: {
     create_issue: {
       executor: "ramping-arrival-rate",
-      exec: "test_create_issue",
+      exec: "testCreateIssue",
       timeUnit: TIME_UNIT,
       preAllocatedVUs: PRE_ALLOCATED_VUS_10,
       stages: [
@@ -64,7 +63,7 @@ export const options = {
     },
     view_issue: {
       executor: "ramping-arrival-rate",
-      exec: "test_view_issue",
+      exec: "testViewIssue",
       timeUnit: TIME_UNIT,
       preAllocatedVUs: PRE_ALLOCATED_VUS_50,
       stages: [
@@ -87,10 +86,13 @@ export const options = {
   },
   // Thresholds defined for specific requests will appear in summary output.
   thresholds: {
-    [`http_req_duration{name:${CREATE_ISSUE}}`]: ["p(90)<1000"],
-    [`http_req_duration{name:${UPDATE_ISSUE}}`]: ["p(90)<1000"],
-    [`http_req_duration{name:${VIEW_ISSUE}}`]: ["p(90)<500"],
-    [`http_req_duration{name:${SEARCH}}`]: ["p(90)<500"],
+    [`http_req_duration{name:${ENDPOINTS["sortedIssues"]["path"]}}`]: [
+      "p(90)<500",
+    ],
+    [`http_req_duration{name:${ENDPOINTS["issuesGetter"]["path"]}}`]: [
+      "p(90)<500",
+    ],
+    [`http_req_duration{name:${ENDPOINTS["issues"]["path"]}}`]: ["p(90)<1000"],
     http_req_failed: ["rate < 0.01"],
   },
 };
@@ -117,7 +119,8 @@ const descriptions = new SharedArray("descriptions", function () {
   }).data;
 });
 
-export function test_create_issue() {
+export function testCreateIssue() {
+  const CREATE_ISSUE = "/issues";
   const token = randomItem(tokens);
   const summary = randomItem(summaries);
   const description = randomItem(descriptions);
@@ -145,30 +148,49 @@ export function test_update_issue() {
   sleep(1);
 }
 
-export function test_view_issue() {
-  sleep(1);
-}
-
-export function test_view_issues() {
-  // GET /api/users/me?$top=-1
-  // GET /api/admin/timeTrackingSettings/workTimeSettings?fields=minutesADay,minutesADayPresentation,workDays,firstDayOfWeek,daysAWeek
-  // GET /hub/api/rest/settings/public?fields=helpdeskEnabled
-  // GET /api/users/me/profiles/grazie?fields=hasMoreTokens,enabled,excludedIssueTypes,cycleRestart,enableSpellChecker,spellCheckerEnabledInSystem,freeLicense
-  // GET /api/admin/globalSettings?fields=restSettings(allowAllOrigins,allowedOrigins),imageTextRecognitionSettings(enabled),systemSettings(ocrSupported)
-  // GET /api/admin/widgets/general?fields=id,key,appId,description,appName,name,collapsed,indexPath,extensionPoint(),iconPath,appIconPath,expectedHeight,expectedWidth
-  // GET /api/permissions/cache?fields=global,permission(key),projects(id)
-  // GET /api/users/me/profiles/questionnaire?fields=show
-  // GET /api/filterFields?$top=-1&fields=$type,id,name,customField(id,name,ordinal,aliases,localizedName,fieldType(id,presentation,isBundleType,valueType,isMultiValue))&getUnusedVisibleFields=true&fieldTypes=custom&query=
-  // GET /api/users/me/recent/issues?fields= (with many params)
-  // GET /api/users/me/recent/articles?fields=
-  // GET /api/search/assist?fields=
-  // !!! GET /api/sortedIssues?topRoot=100&skipRoot=0&flatten=true&query=&fields=tree(id,summaryTextSearchResult(highlightRanges(startOffset,endOffset)))
-  // GET /api/savedQueries?fields=id,issuesUrl,name,query,pinned,pinnedInHelpdesk,isUpdatable,isDeletable,isShareable,owner(id,ringId,login,name,email,isEmailVerified,guest,fullName,avatarUrl,online,banned,banBadge,canReadProfile,isLocked,userType(id))&sort=true&$top=-1
-  // x8 GET /api/filterFields/project/values?$top=-1&fields=id,presentation,query&prefix=&query=&type=Issue
-  // !!! POST /api/issuesGetter?$top=-1&$skip=0&fields= (top 100 ids payload)
-  // POST /api/issuesGetter/counts?fields=counts(folder(id),count)
-  // POST /api/issuesGetter/count?fields=folder(id),count
-  // POST /api/issueListSubscription?fields=ticket (top 100 ids payload)
+export function testViewIssue() {
+  const token = randomItem(tokens);
+  const params = {
+    tags: {},
+    timeout: HTTP_TIMEOUT,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  // View Issues
+  params.tags.name = GET_PREFIX + ENDPOINTS["sortedIssues"]["path"];
+  const res = http.get(
+    `${BASE_URL}/${API_PREFIX}${ENDPOINTS["sortedIssues"]["path"]}?${ENDPOINTS["sortedIssues"]["params"]}`,
+    params,
+  );
+  const payload = JSON.stringify({ query: "" });
+  params.tags.name = POST_PREFIX + ENDPOINTS["issuesGetterCount"]["path"];
+  http.post(
+    `${BASE_URL}/${API_PREFIX}${ENDPOINTS["issuesGetterCount"]["path"]}?${ENDPOINTS["issuesGetterCount"]["params"]}`,
+    payload,
+    params,
+  );
+  if (res.status === 200) {
+    const topIssue = res.json().tree.map((item) => ({
+      id: item.id,
+    }));
+    const payload = JSON.stringify(topIssue);
+    params.tags.name = POST_PREFIX + ENDPOINTS["issuesGetter"]["path"];
+    http.post(
+      `${BASE_URL}/${API_PREFIX}${ENDPOINTS["issuesGetter"]["path"]}?${ENDPOINTS["issuesGetter"]["params"]}`,
+      payload,
+      params,
+    );
+    // View Issue
+    sleep(3);
+    const issueId = randomItem(topIssue).id.split("-")[1];
+    params.tags.name = GET_PREFIX + ENDPOINTS["issues"]["path"];
+    http.get(
+      `${BASE_URL}/${API_PREFIX}${ENDPOINTS["issues"]["path"]}/DEMO-${issueId}?${ENDPOINTS["issues"]["params"]}`,
+      params,
+    );
+  }
 }
 
 export function test_search() {
@@ -176,8 +198,8 @@ export function test_search() {
 }
 
 export default function () {
-  test_create_issue();
-  test_update_issue();
-  test_view_issue();
-  test_search();
+  testCreateIssue();
+  //test_update_issue();
+  testViewIssue();
+  //test_search();
 }
