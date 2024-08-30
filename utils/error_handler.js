5ec29996ff2d53cc6f0e9722b4ec7eaf79dbe3e5
Yury Suponeu <usuponev@gmail.com>
2024-08-30 17:32:08 +0400

    [utils] remove unused error tracing

--- utils/error_handler.js
+++ utils/error_handler.js
@@ -9,17 +9,11 @@ export class ErrorHandler {
   // Logs response error details if isError is true.
   logError(isError, res, tags = {}) {
     if (!isError) return;
-
-    // the Traceparent header is a W3C Trace Context
-    const traceparentHeader = res.request.headers["Traceparent"];
-
-    // Add any other useful information
     const errorData = Object.assign(
       {
         url: res.url,
         status: res.status,
         error_code: res.error_code,
-        traceparent: traceparentHeader && traceparentHeader.toString(),
       },
       tags,
     );
